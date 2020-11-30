d9a15ed8ab5f85adb7b95698ed4bf57718bfc34c
DrewMHurst <freelancetechservices@gmail.com>
2020-11-29 23:47:31 -0700

    Commit Message

--- main_calculator.js
+++ main_calculator.js
@@ -18,6 +18,7 @@
 const calculator = document.querySelector('.calculator')
 const keys = calculator.querySelector('.calculator_keys')
 const display = calculator.querySelector('.calculator_display')
+const operatorKeys = keys.querySelectorAll('[data-type="operator"]')
 
 // For the const keys & display you can also make them equal to document as shwon below
 //// const keys = document.querySelector('.calculator_keys')
@@ -37,24 +38,22 @@ keys.addEventListener('click', event => {
 
 //is this a number key?
 
-if (type === 'number'){
+if (type === 'number') {
 
-  if (displayValue === '0'){
-    display.textContent = keyValue
-
-  } else if (previousKeyType === 'operator')
-  {
+  if (
+    displayValue === '0' ||
+    previousKeyType === 'operator'
+  ) {
     display.textContent = keyValue
   } else {
     display.textContent = displayValue + keyValue
   }
-
 }
 
 //is this an operator key?
 
-if (type === 'operator'){
-const operatorKeys = keys.querySelectorAll('[data-type="operator"]')
+if (type === 'operator') {
+
 operatorKeys.forEach(el => {el.dataset.state = '' })
 key.dataset.state = 'selected'
 
@@ -69,9 +68,8 @@ calculator.dataset.operator = key.dataset.key
 
 }
 
-
 //perform a calculation
-if (type === 'equal'){
+if (type === 'equal') {
 
 const firstNumber = calculator.dataset.firstNumber
 const operator = calculator.dataset.operator
@@ -79,6 +77,12 @@ const secondNumber = displayValue
 //console.log(firstNumber, operator, secondNumber)
 display.textContent = calculate(firstNumber, operator, secondNumber)
 
+}
+
+if (type === 'clear') {
+display.textContent = '0'
+delete calculator.dataset.firstNumber
+delete calculator.dataset.operator
 }
 
 calculator.dataset.previousKeyType = type
@@ -104,6 +108,91 @@ function calculate (firstNumber, operator, secondNumber) {
 //  case 'divide': result = firstNumber / secondNumber; break
 //  }
 
-return result.toFixed(2)
+//return result.toFixed(2)
 //.toFixed(2) can be used to have rounding for switch
 }
+
+//==============================
+///Testing Calculator
+//==============================
+
+//console.assert() a method writes an error message to the console if the assertion is false.
+//If the assertion is true, nothing happens.
+
+// console.assert(1 !== 1, 'one is not one') web console should say: Assertion failed: one is one main_calculator.js:116:9
+function clearCalculator () {
+// press the clear key
+const clearKey = document.querySelector('[data-type="clear"]')
+clearKey.click()
+
+// Clear operator states
+ operatorKeys.forEach(key => { key.dataset.state = '' })
+}
+
+function testClearKey () {
+  clearCalculator()
+  console.assert(display.textContent === '0', 'Clear key. Display should be 0')
+  console.assert(!calculator.dataset.firstNumber, 'Clear key. No number remains')
+  console.assert(!calculator.dataset.operator, 'Clear key. No operator remains')
+}
+
+const one = document.querySelector('[data-key="1"]')
+const five = document.querySelector('[data-key="5"]')
+const nine = document.querySelector('[data-key="9"]')
+
+function testKeySequence (test) {
+//const array = [...keys]
+// Press keys
+// array.forEach(key => {
+  test.keys.forEach(key => {
+// Press one keys
+// `` vs ''
+  document.querySelector(`[data-key="${key}"]`).click()
+
+  })
+
+// Assertion
+// 1. Value to assert
+// 2. Test message
+console.assert(display.textContent === test.value, test.message)
+
+// Clear calculation
+clearCalculator()
+testClearKey()
+}
+
+const tests = [{
+  keys: ['1'],
+  value: '1',
+  message: 'Click 1'
+}, {
+  keys: ['1', '5'],
+  value: '15',
+  message: 'Click 15'
+}, {
+  keys: ['1', '5', '9'],
+  value: '159',
+  message: 'Click 159'
+}, {
+  keys: ['2', '4', 'plus', '7', 'equal'],
+  value: '31',
+  message: 'Calculation with plus'
+}, {
+  keys: ['3', 'minus', '7', '0', 'equal'],
+  value: '-67',
+  message: 'Calculation with minus'
+}, {
+  keys: ['1', '5', 'times', '9', 'equal'],
+  value: '135',
+  message: 'Calculation with times'
+}, {
+  keys: ['9', 'divide', '3', 'equal'],
+  value: '3',
+  message: 'Calculation with divide'
+}, {
+  keys: ['9', 'divide', '0', 'equal'],
+  value: 'Infinity',
+  message: 'Calculation. Divide by 0'
+}]
+// testKeySequence('1', '5', '9')
+tests.forEach(testKeySequence)
